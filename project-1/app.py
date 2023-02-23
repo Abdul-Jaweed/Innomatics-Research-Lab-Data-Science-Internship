1fa74eadbb56da925ee2bc8d9e1d2575a492c47b
Abdul Jaweed <jdgaming7320@gmail.com>
2023-02-23 18:53:22 +0530

    add first project

---
+++ project-1/app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+# title
+
+st.title("Innomatics Research Lab")
+
+# header / sub header
+
+st.header("Data Science Internship")
+
+# st.subheader("Feb-2023")
+
+
+
+st.markdown("## Candidate name : Abdul Jaweed")
+st.markdown("## Batch no : Feb-2023")
+
+st.write("## Follow me on Linkedin [link](https://www.linkedin.com/in/abdul-jaweed-8a840a238/)")
+st.write("## Github [link](https://github.com/Abdul-Jaweed)")
+
